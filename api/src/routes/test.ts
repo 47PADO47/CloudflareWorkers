09449fdda95d65dcf6576b44e0e21c4cb503e2b6
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2022-05-21 13:12:44 +0200

    ✅ Added DiscordWebhook test route

--- api/src/routes/test.ts
+++ api/src/routes/test.ts
@@ -1,4 +1,5 @@
 import BaseRoute from '../utils/BaseRoute';
+import { DiscordWebhook } from '../utils/Utils';
 
 class TestRoute extends BaseRoute {
     constructor() {
@@ -8,9 +9,32 @@ class TestRoute extends BaseRoute {
     }
 
     static async handle(request: Request): Promise<Response> {
-        return new Response(JSON.stringify({
-            message: "'/test' working"
-        }), { status: 200 });
+
+        const url = new URL(request.url);
+        const path = url.pathname.split(`/${this.name}`)[1];
+
+        switch (path) {
+            case "":
+                return new Response(JSON.stringify({
+                    message: "'/test' working"
+                }), { status: 200 });
+            case "/sendWebhook":
+                const status = await DiscordWebhook({
+                    title: "Test Call",
+                    description: "This is a test!",
+                });
+
+                return new Response(JSON.stringify({
+                    message: "Webhook sent!",
+                    status,
+                }), { status: 200 });
+                
+            default:
+                return new Response(JSON.stringify({
+                    error: `'/${this.name}${path}' not found`,
+                    code: 404,
+                }), { status: 404 });
+        };
     };
 };
 
