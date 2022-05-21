6ba43dcec1d6cc10568c0de9723b90a9f39bbc88
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2022-05-21 14:16:26 +0200

    🛂 Update Constants.ts

--- api/src/utils/Constants.ts
+++ api/src/utils/Constants.ts
@@ -1,7 +1,14 @@
+import { Admin } from "../types/Constants";
+
 const DB_URL = ""; // Replace with your database url (mongoDB realm)
 const DISCORD_WEBHOOK = ""; // Replace with your discord webhook url
+const ADMINS = [{
+    username: "",
+    password: "",
+}] as Admin[]; // Replace with your admin accounts
 
 export {
     DB_URL,
     DISCORD_WEBHOOK,
+    ADMINS,
 };
